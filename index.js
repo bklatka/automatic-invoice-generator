1b1f046c5fefb428649f2d6e324f5bb3b32b86e1
Bernard Klatka <bklatka@evolution.com>
2023-02-17 10:52:42 +0100

    feat: add auatomatic invoice generator

---
+++ index.js
@@ -0,0 +1,87 @@
+const axios = require('axios');
+const fs = require('fs');
+require('dotenv').config();
+const path = require('path');
+const os = require('os');
+
+
+
+
+
+const { INFAKT_API_KEY, SELLER_NAME, SELLER_NIP, SELLER_BANK_ACOCUNT, SERVICE_NAME, CLIENT_ID } = process.env;
+const NET_PRICE = parseFloat(process.env.NET_PRICE);
+const YOUR_DATA = {
+    name: SELLER_NAME,
+    tax_no: SELLER_NIP,
+    bank_account: SELLER_BANK_ACOCUNT,
+    serviceName: SERVICE_NAME
+}
+
+
+
+// Ustawienia Infakt API
+const infaktApiKey = INFAKT_API_KEY;
+const infaktUrl = 'https://api.infakt.pl/v3';
+
+const FILE_NAME = `${new Date().getMonth() + 1}.pdf`;
+
+const filePath = path.join(os.homedir(), 'Desktop', FILE_NAME);
+
+
+
+// Ustawienia faktury
+const today = new Date();
+const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+const paymentDate = new Date(today.getTime() + (14 * 24 * 60 * 60 * 1000));
+const invoiceData = {
+    invoice_date: lastDayOfMonth.toISOString().slice(0, 10), // Data wystawienia faktury
+    sale_date: lastDayOfMonth.toISOString().slice(0, 10), // Data wystawienia faktury
+    payment_date: paymentDate.toISOString().slice(0, 10), // Data płatności
+    seller_name: YOUR_DATA.name, // Nazwa sprzedawcy
+    seller_tax_no: YOUR_DATA.tax_no, // NIP sprzedawcy
+    seller_bank_account: YOUR_DATA.bank_account, // Numer konta bankowego sprzedawcy
+    client_id: CLIENT_ID,
+    services: [ // Pozycje na fakturze
+        {
+            name: YOUR_DATA.serviceName,
+            quantity: 1,
+            unit: 'szt.',
+            net_price: NET_PRICE * 100,
+            tax_price: NET_PRICE * 0.23 * 100,
+            gross_price: NET_PRICE * 1.23 * 100, // gross price in grosze
+            tax_symbol: 23,
+        },
+    ],
+};
+
+// Tworzenie faktury w Infakt
+axios.post(`${infaktUrl}/invoices.json`, { invoice: invoiceData }, {
+    headers: {
+        "X-inFakt-ApiKey": infaktApiKey,
+        'Content-Type': 'application/json',
+    },
+})
+    .then((response) => {
+        const invoiceId = response.data.id;
+
+        axios({
+            method: 'get',
+            url: `${infaktUrl}/invoices/${invoiceId}/pdf.json`,
+            responseType: "arraybuffer",
+            data: {
+                document_type: "original"
+            },
+            headers: {
+                "X-inFakt-ApiKey": infaktApiKey,
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(response => {
+                fs.writeFileSync(filePath, response.data);
+                console.log(`Zapisano fakturę w ${filePath}`);
+            })
+
+    })
+    .catch((error) => {
+        console.error(error);
+    });
