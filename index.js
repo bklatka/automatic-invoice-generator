94ebb1e55dda4a4633a4ecd940af7c83a460d20d
Bernard Klatka <bklatka@evolution.com>
2023-03-01 18:19:59 +0100

    feat: add option to select month and fix payment date. Use date-fns

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const fs = require('fs');
 require('dotenv').config();
 const path = require('path');
 const os = require('os');
+const { addDays, endOfMonth } = require('date-fns');
+
+const monthChooser = require('./selectMonth');
 
 
+
+function getEndOfMonthDay(monthNumber) {
+    // Check if the month number is valid
+    if (monthNumber >= 1 && monthNumber <= 12) {
+        // Create a date object for the end of the month
+        const endOfMonthDate = endOfMonth(new Date(2023, monthNumber - 1));
+        // Add 14 days to the end of the month
+        return endOfMonthDate;
+    } else {
+        throw new Error('Invalid month number.');
+    }
+}
 
 
 
@@ -29,59 +44,67 @@ const filePath = path.join(os.homedir(), 'Desktop', FILE_NAME);
 
 
 
+
+monthChooser.selectMonth().then(month => {
+
 // Ustawienia faktury
-const today = new Date();
-const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-const paymentDate = new Date(today.getTime() + (14 * 24 * 60 * 60 * 1000));
-const invoiceData = {
-    invoice_date: lastDayOfMonth.toISOString().slice(0, 10), // Data wystawienia faktury
-    sale_date: lastDayOfMonth.toISOString().slice(0, 10), // Data wystawienia faktury
-    payment_date: paymentDate.toISOString().slice(0, 10), // Data płatności
-    seller_name: YOUR_DATA.name, // Nazwa sprzedawcy
-    seller_tax_no: YOUR_DATA.tax_no, // NIP sprzedawcy
-    seller_bank_account: YOUR_DATA.bank_account, // Numer konta bankowego sprzedawcy
-    client_id: CLIENT_ID,
-    services: [ // Pozycje na fakturze
-        {
-            name: YOUR_DATA.serviceName,
-            quantity: 1,
-            unit: 'szt.',
-            net_price: NET_PRICE * 100,
-            tax_price: NET_PRICE * 0.23 * 100,
-            gross_price: NET_PRICE * 1.23 * 100, // gross price in grosze
-            tax_symbol: 23,
-        },
-    ],
-};
+    const lastDayOfMonth = getEndOfMonthDay(month);
+    const paymentDate = addDays(lastDayOfMonth, 14);
+    const invoiceData = {
+        invoice_date: lastDayOfMonth.toISOString().slice(0, 10), // Data wystawienia faktury
+        sale_date: lastDayOfMonth.toISOString().slice(0, 10), // Data wystawienia faktury
+        payment_date: paymentDate.toISOString().slice(0, 10), // Data płatności
+        seller_name: YOUR_DATA.name, // Nazwa sprzedawcy
+        seller_tax_no: YOUR_DATA.tax_no, // NIP sprzedawcy
+        seller_bank_account: YOUR_DATA.bank_account, // Numer konta bankowego sprzedawcy
+        client_id: CLIENT_ID,
+        services: [ // Pozycje na fakturze
+            {
+                name: YOUR_DATA.serviceName,
+                quantity: 1,
+                unit: 'szt.',
+                net_price: NET_PRICE * 100,
+                tax_price: NET_PRICE * 0.23 * 100,
+                gross_price: NET_PRICE * 1.23 * 100, // gross price in grosze
+                tax_symbol: 23,
+            },
+        ],
+    };
+
 
 // Tworzenie faktury w Infakt
-axios.post(`${infaktUrl}/invoices.json`, { invoice: invoiceData }, {
-    headers: {
-        "X-inFakt-ApiKey": infaktApiKey,
-        'Content-Type': 'application/json',
-    },
-})
-    .then((response) => {
-        const invoiceId = response.data.id;
-
-        axios({
-            method: 'get',
-            url: `${infaktUrl}/invoices/${invoiceId}/pdf.json`,
-            responseType: "arraybuffer",
-            data: {
-                document_type: "original"
-            },
-            headers: {
-                "X-inFakt-ApiKey": infaktApiKey,
-                'Content-Type': 'application/json',
-            }
-        })
-            .then(response => {
-                fs.writeFileSync(filePath, response.data);
-                console.log(`Zapisano fakturę w ${filePath}`);
-            })
-
+    axios.post(`${infaktUrl}/invoices.json`, { invoice: invoiceData }, {
+        headers: {
+            "X-inFakt-ApiKey": infaktApiKey,
+            'Content-Type': 'application/json',
+        },
     })
-    .catch((error) => {
-        console.error(error);
-    });
+        .then((response) => {
+            const invoiceId = response.data.id;
+
+            axios({
+                method: 'get',
+                url: `${infaktUrl}/invoices/${invoiceId}/pdf.json`,
+                responseType: "arraybuffer",
+                data: {
+                    document_type: "original"
+                },
+                headers: {
+                    "X-inFakt-ApiKey": infaktApiKey,
+                    'Content-Type': 'application/json',
+                }
+            })
+                .then(response => {
+                    fs.writeFileSync(filePath, response.data);
+                    console.log(`Zapisano fakturę w ${filePath}`);
+                })
+
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+
+
+
+})
+
